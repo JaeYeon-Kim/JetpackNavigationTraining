a4f6dfe091c5176dc1d904aa805e50b781bd119f
jaeYoun <rlawodusx@naver.com>
2023-02-08 22:35:19 +0900

    [feat: Jetpack Navigation 기능 복습]

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,48 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // 프래그먼트에서 데이터 바인딩 개체 선언
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment2_to_gameFragment) }
+        // 사용자가 메뉴 항목을 탭할때 동작을 구현하는 코드 추가
+        setHasOptionsMenu(true)
+        return binding.root
+        }
+
+    // 미리 만든 menu 리소스를 해당 메서드를 통해서 가져 온다.
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+
+    }
+    // 옵션 선택시 작업 수행을 위한 메서드
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+                onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+}
